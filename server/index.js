386475041ef472edb83c3b073aa53a2d4b06d474
Monilito Castro <mannycastro.epic@gmail.com>
2021-08-07 14:24:07 -0400

    first

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 9090
+app.use(cors())
+app.get('/eom-auth/get-users', (req, res) => {
+    const data = [
+        {
+            id:1,
+            text: "asdf1"
+        },
+        {
+            id: 2,
+            text: "asdf2"
+        }
+    ]
+  res.send(JSON.stringify(data))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
